7de5101b3f6cef2da37c2ef39e15f0a64dc1ebd9
gsmerlin <gabrielsmerlin@outlook.com>
2022-10-27 16:19:01 -0300

    Added dumbass brazillian version

--- src/components/Component1/index.tsx
+++ src/components/Component1/index.tsx
@@ -5,7 +5,7 @@ import './styles.css'
 function Component1() {
     return (
         <div>
-            <img src={"https://compote.slate.com/images/4c7420ed-9afa-4d6a-a7c1-3c45a8bcaa14.jpeg?width=780&height=520&rect=1560x1040&offset=0x0"} alt={"booo"}/>
+            <img src={"img.png"} alt={"what an idiot"}/>
         </div>
     );
 }
